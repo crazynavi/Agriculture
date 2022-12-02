dae4ac574283bf032d321890e03b8a77ebeb8a87
crazynavi <116185190+crazynavi@users.noreply.github.com>
2022-12-02 03:33:41 -0500

    add expire function

--- src/App.js
+++ src/App.js
@@ -18,16 +18,36 @@ import { useEffect } from "react";
 
 function App() {
   const { token, setToken } = useToken();
+  // useEffect(() => {
+  //   if (localStorage.getItem("tokenData")) {
+  //     const userRole = JSON.parse(localStorage.getItem("tokenData")).user_role;
+  //   }
+  // }, [token]);
   useEffect(() => {
-    if (localStorage.getItem("tokenData")) {
-      const userRole = JSON.parse(localStorage.getItem("tokenData")).user_role;
+    const TIMESTAMP = Date.now();
+    if (token) {
+      console.log("fewfwe");
+      if (!JSON.parse(localStorage.getItem("timestamp"))) {
+        localStorage.setItem("timestamp", JSON.stringify({
+          initial: TIMESTAMP,
+          expiresOn: TIMESTAMP + 1000 * 60 * 60 //1hour
+        }));
+      } else {
+        // then, when user access the website again, check the expiresOn, it it's value is bigger than current date
+        const EXPIRE_DATE = JSON.parse(localStorage.getItem("timestamp")).expiresOn;
+
+        if (Date.now() > EXPIRE_DATE) {
+          localStorage.removeItem("tokenData");
+          localStorage.removeItem("timestamp");
+          window.location.href = "";
+        }
+      }
     }
-  }, [token]);
+  })
 
   if (!token) {
     return <Login setToken={setToken} />;
   }
-
   return (
     <div className="main-container">
       <HashRouter>
--- src/utils/isRoleAllowed.js
+++ src/utils/isRoleAllowed.js
@@ -1,7 +1,7 @@
 export const isRoleAllowed = (list) => {
-  // const role = JSON.parse(localStorage.getItem("tokenData")).user_role;
+  const role = JSON.parse(localStorage.getItem("tokenData")).user_level;
     // let role = [2656,2658,2669,2771,2663,2666,2635,2644,2650,2652,2734];
-    let role = [2656,2666,2635,2644,2734];
+    // let role = [2656,2666,2635,2644,2734];
   let isAllowed = false;
   role.forEach((element) => {
     if (list.indexOf(element) > -1) isAllowed = true;
